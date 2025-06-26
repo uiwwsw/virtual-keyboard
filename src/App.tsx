353babf08825cd49966ffce81f6f44e27ffd41bf
uiwwsw <uiwwsw@icloud.com>
2025-06-26 22:33:07 +0900

    feat: save

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,10 @@
-import { useRef, type ChangeEvent } from "react";
+import {
+	useRef,
+	useState,
+	type ChangeEvent,
+	type FocusEvent,
+	type MouseEvent,
+} from "react";
 
 import { ComposedInput } from "./ComposedInput";
 import { generateCrossword } from "./generateCrossword";
@@ -8,11 +14,12 @@ const myWords = [
 	"알고리즘",
 	"크로스워드",
 	"리액트",
-	"자바스크립트",
+	"자바스크립트입니다",
 	"개발자",
-	"즘스",
-	"호두",
 	"알리",
+	"타바나",
+	"립제이",
+	"이거뭔데",
 ];
 
 const crosswordMap = generateCrossword(myWords);
@@ -22,30 +29,60 @@ const coll = crosswordMap[0].length ?? 1;
 function Input({
 	index,
 	wrapRef,
+	direct,
 	answer,
+	onToggle,
 }: {
+	onToggle: () => unknown;
+	direct: boolean;
 	answer?: string;
 	index: number;
 	wrapRef: React.RefObject<HTMLDivElement | null>;
 }) {
 	const ref = useRef<HTMLInputElement>(null);
+	// const handleClick = (e: React.MouseEvent<HTMLInputElement>) => {
+	// 	// 현재 클릭된 input
+	// 	const input = e.currentTarget;
+
+	// 	if (document.activeElement === input) {
+	// 		onToggle();
+	// 	}
+	// };
+	const handleFocus = (e: FocusEvent<HTMLInputElement>) => {
+		if (e.target.dataset.id === "empty") return;
+
+		if (e.target.readOnly) {
+			const nextInput = wrapRef.current?.children[index + (direct ? 1 : coll)];
+			if (nextInput instanceof HTMLInputElement)
+				if (nextInput.dataset.id !== "empty") nextInput.focus();
+		}
+	};
 	const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
 		if (answer === e.target.value) {
 			if (ref.current) {
 				ref.current.readOnly = true;
 				ref.current.style.background = "red";
-				const nextInput = wrapRef.current?.children[index + 1];
-				if (
-					nextInput instanceof HTMLInputElement &&
-					nextInput.dataset.id !== "empty"
-				)
-					nextInput.focus();
+				const nextInput =
+					wrapRef.current?.children[index + (direct ? 1 : coll)];
+				if (nextInput instanceof HTMLInputElement) {
+					if (nextInput.dataset.id !== "empty") nextInput.focus();
+					else {
+						onToggle();
+						(
+							wrapRef.current?.children[
+								index + (!direct ? 1 : coll)
+							] as HTMLInputElement
+						).focus();
+					}
+				}
 			}
 		}
 	};
 	return (
 		<ComposedInput
 			ref={ref}
+			onFocus={handleFocus}
+			placeholder={answer}
 			disabled={!answer}
 			data-id={answer ? null : "empty"}
 			onChange={handleChange}
@@ -56,6 +93,7 @@ function Input({
 }
 
 function App() {
+	const [direct, setDirect] = useState(true);
 	const ref = useRef<HTMLDivElement>(null);
 	return (
 		<>
@@ -65,6 +103,8 @@ function App() {
 						const key = `${i}-${j}`;
 						return (
 							<Input
+								onToggle={() => setDirect(!direct)}
+								direct={direct}
 								answer={y}
 								index={i * coll + j}
 								key={y ? key : "empty"}
