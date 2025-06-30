7661789f16c623a5350dd92214d85d8059627ae4
uiwwsw <uiwwsw@hankookilbo.com>
2025-06-30 13:14:50 +0900

    test: es-hangul

--- src/ComposedInput.tsx
+++ src/ComposedInput.tsx
@@ -1,45 +1,48 @@
+import { disassemble } from "es-hangul";
 import { forwardRef, type InputHTMLAttributes, useRef } from "react";
 export type ComposedInputProps = InputHTMLAttributes<HTMLInputElement>;
 export const ComposedInput = forwardRef<HTMLInputElement, ComposedInputProps>(
-	function ComposedInput(
-		{
-			onChange,
-			onCompositionStart,
-			onCompositionUpdate,
-			onCompositionEnd,
-			...props
-		},
-		ref,
-	) {
-		const isCmpositionRef = useRef<true>(null);
+  function ComposedInput(
+    {
+      onChange,
+      onCompositionStart,
+      onCompositionUpdate,
+      onCompositionEnd,
+      ...props
+    },
+    ref
+  ) {
+    const isCmpositionRef = useRef<true>(null);
 
-		return (
-			<input
-				{...props}
-				onChange={(e) => {
-					if (isCmpositionRef.current === null) {
-						e.preventDefault();
-						e.stopPropagation();
-						e.target.value = ''
-						return;
-					}
-					onChange?.(e);
-				}}
-				onCompositionStart={(e) => {
-					isCmpositionRef.current = true;
-					onCompositionStart?.(e);
-				}}
-				onCompositionUpdate={(e) => {
-					isCmpositionRef.current = true;
-					onCompositionUpdate?.(e);
-				}}
-				onCompositionEnd={(e) => {
-					isCmpositionRef.current = true;
-					onCompositionEnd?.(e);
-				}}
-				ref={ref}
-			/>
-		);
-	},
+    return (
+      <input
+        {...props}
+        onChange={(e) => {
+          if (isCmpositionRef.current === null) {
+            e.preventDefault();
+            e.stopPropagation();
+            const newValue = disassemble(e.target.value);
+            if (newValue !== e.target.value) e.target.value = "";
+
+            return;
+          }
+          onChange?.(e);
+        }}
+        onCompositionStart={(e) => {
+          isCmpositionRef.current = true;
+          onCompositionStart?.(e);
+        }}
+        onCompositionUpdate={(e) => {
+          isCmpositionRef.current = true;
+          onCompositionUpdate?.(e);
+        }}
+        onCompositionEnd={(e) => {
+          isCmpositionRef.current = true;
+          onCompositionEnd?.(e);
+        }}
+        ref={ref}
+      />
+    );
+  }
 );
 ComposedInput.displayName = "ComposedInput";
