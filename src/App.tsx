e50041eecb68c1252b7b28b6efed84904b8da6c2
uiwwsw <uiwwsw@hankookilbo.com>
2025-07-03 08:59:49 +0900

    feat: 윈도우 한글 지원

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Input } from "./components/Input";
 
 function App() {
-	return (
-		<>
-			<Input>가나다</Input>
-		</>
-	);
+  return (
+    <>
+      <Input>가나다</Input>
+      <div>dwadawd</div>
+      <input type="text" />
+    </>
+  );
 }
 
 export default App;
