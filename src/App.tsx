f24b8b35f3af8e305358288245635ce6cfb5d836
uiwwsw <uiwwsw@icloud.com>
2025-07-16 00:16:01 +0900

    fix: 폰번호입력 layout 추가

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,31 @@ function App() {
 		<>
 			<VirtualInputProvider>
 				<VirtualInput initialValue="가나달" />
-				<VirtualInput initialValue="가나달" />
+			</VirtualInputProvider>
+			<VirtualInputProvider
+				layout={[
+					[
+						{ value: "1", label: "1" },
+						{ value: "2", label: "2" },
+						{ value: "3", label: "3" },
+					],
+					[
+						{ value: "4", label: "4" },
+						{ value: "5", label: "5" },
+						{ value: "6", label: "6" },
+					],
+					[
+						{ value: "7", label: "7" },
+						{ value: "8", label: "8" },
+						{ value: "9", label: "9" },
+					],
+					[
+						{ value: "010", label: "010" },
+						{ value: "0", label: "0" },
+						{ label: "del", value: "Backspace", type: "action" },
+					],
+				]}
+			>
 				<VirtualInput initialValue="가나달" />
 			</VirtualInputProvider>
 			<div>dwadawd</div>
